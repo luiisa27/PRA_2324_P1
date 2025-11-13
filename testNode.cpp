93af38a317c59f89bdedc4e62073805feb258d02
Luisa Arango <lfarator@etsinf.upv.es>
2025-11-13 12:51:28 +0100

    añadido codigo de test de la lcase node y makefile actulaizado

---
+++ testNode.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Node.h"
+using namespace std;
+
+
+int main(){
+    Node<char>* first = new Node<char>('A');
+    first = new Node<char>('R', first);
+    first = new Node<char>('P', first);
+
+    cout << "Secuencia: ";
+    Node<char>* aux = first;
+    while (aux != nullptr){
+        cout << aux->data << " ";
+        aux = aux->next;
+    }
+    cout << endl;
+}
